38b0559dddcbc8cf046702faeaa8160832922e17
Mathieu BARRY <mathieu.b@make-it.fr>
2025-04-05 19:43:56 +0200

    fix: remove bad file

---
+++ assets/react/controllers/Hello.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function (props) {
+    return (
+        <>
+            <h1>Hello {props.fullName}</h1>
+            <p>J'ai enfin réussi à faire mon app à la con</p>
+        </>
+    );
+}
